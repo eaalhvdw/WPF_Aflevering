12443eb8591143f9d7ad60b020c426e39d26c0bc
eaalhvdw <louielouie5@msn.com>
2020-03-06 11:23:44 +0100

    Done

---
+++ WpfAssignment/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WpfAssignment
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * Shows a file dialog and reads a csv file the user has selected.
+         * Reads the csv file with the user class method ReadCSVFile 
+         * that takes the string filename as an argument to access the file.
+         * The listbox is filled with the user objects and all the info
+         * of the selected object in the list is shown in bound labels
+         * in the inner grid to the right of the listbox.
+         * Then the statusbar textblocks are updated to 
+         * show how many items are in the listbox and at 
+         * what time the users were loaded into the listbox.
+         */
+        private void openFileMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // Setup file dialog
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".csv";
+            fileDialog.Filter = "CSV Files (.csv)|*.csv";
+
+            // Show file dialog.
+            Nullable<bool> result = fileDialog.ShowDialog();
+
+            // Process result.
+            if(result == true)
+            {
+                // Load users from file into a list.
+                string filename = fileDialog.FileName;
+                List<User> users = User.ReadCSVFile(filename);
+                
+                // Load list of users into the listbox.
+                listBox.ItemsSource = users; 
+                
+                // Show all user information of the selected user to the right of the listbox.
+                gridInner.DataContext = users;
+
+                // Set statusbar textblock with number of lines in the listbox.
+                int countLines = listBox.Items.Count;
+                txtNumLinesStatus.Text = countLines + " Items";
+                
+                // Set statusbar textblock timestamp for loading users into listbox.
+                var timestamp = DateTime.Now.ToLongTimeString();
+                txtTimestampStatus.Text = "Last users loaded:\t" + timestamp;
+            }
+        }
+    }
+}
---
+++ WpfAssignment/User.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WpfAssignment
+{
+    public class User
+    {
+        /* 
+         * Constructor takes a string as argument 
+         * in the format: id;name;age;score.
+         */
+        public User(string str)
+        {
+            var result = str.Split(';');
+            Id = int.Parse(result[0]);
+            Name = result[1];
+            Age = int.Parse(result[2]);
+            Score = int.Parse(result[3]);
+        }
+
+        // Properties.
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Score { get; set; }
+
+        /*
+         * ToString method for ListBox only 
+         * shows id and name of user objects.
+         */
+        public string ListBoxToString
+        {
+            get
+            {
+                return "Id: " + Id + "\t\tName: " + Name;
+            }
+        }
+
+        /*
+         * This method takes a filename(filepath) and 
+         * reads the file line by line. A user object 
+         * is created for each line and all user objects 
+         * are saved in a list.
+         * Returns a list of user objects.
+         */
+        public static List<User> ReadCSVFile(string filename)
+        {
+            List<User> usersRead = new List<User>();
+
+            using (var file = new StreamReader(filename))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var u = new User(line);
+                    usersRead.Add(u);
+                }
+            }
+            return usersRead;
+        }
+    }
+}
